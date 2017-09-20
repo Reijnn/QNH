15272848d7d36daec4d6d6b7be4251f53d39834a
Reijnn <NTR23git!>
2017-09-20 10:42:11 +0200

    Beautify code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import firebase from './firebase.js';
 
+//const storage = firebase.storage();
+const database = firebase.database();
+
 class App extends Component {
+
   constructor() {
     super();
     this.state = {
+      name: '',
       email: ''
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this); 
+    this.handleChange = this
+      .handleChange
+      .bind(this);
+    this.handleSubmit = this
+      .handleSubmit
+      .bind(this);
   }
 
   handleChange(e) {
@@ -21,8 +30,10 @@ class App extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
+    const itemsRef = database.ref('clients');
+
     const item = {
+      name: this.state.name,
       email: this.state.email
     }
     itemsRef.push(item);
@@ -31,32 +42,40 @@ class App extends Component {
       console.log(snapshot.val());
     });
 
-    this.setState({
-      email: ''
-    });
+    this.setState({name: '', email: ''});
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to QNH</h2>
         </div>
         <div className='container'>
-        <section className='add-item'>
+          <section className='add-item'>
             <form onSubmit={this.handleSubmit}>
-              <input type="text" name="email" placeholder="Wat is je email?" onChange={this.handleChange} value={this.state.email} />
+              <input
+                type="text"
+                name="name"
+                placeholder="Volledige naam"
+                onChange={this.handleChange}
+                value={this.state.name}/>
+              <input
+                type="text"
+                name="email"
+                placeholder="Wat is je email?"
+                onChange={this.handleChange}
+                value={this.state.email}/>
               <button>Versturen</button>
             </form>
-        </section>
-        <section className='display-item'>
-          <div className='wrapper'>
-            <ul>
-            </ul>
-          </div>
-        </section>
-      </div>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul></ul>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
