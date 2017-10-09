0b0f7260275f5a541d3aed7c1113d80779b99ed5
Niels Reijn <niels@Nielss-MacBook-Pro.local>
2017-10-09 14:55:57 +0200

    Remove Firebase Config

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,14 +1,6 @@
 import firebase from 'firebase';
 
-const config = {
-  apiKey: "AIzaSyAdwEXpdSJ0iY1xwje5R3Vbv8c3a2APkfM",
-  authDomain: "qnh-app.firebaseapp.com",
-  databaseURL: "https://qnh-app.firebaseio.com",
-  projectId: "qnh-app",
-  storageBucket: "qnh-app.appspot.com",
-  messagingSenderId: "1027880630848"
-};
-firebase.initializeApp(config);
+//config
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
