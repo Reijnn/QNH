635a28b7cdfd91591fbd2425a22d61e3aed50ecc
Niels Reijn <niels@Nielss-MacBook-Pro.local>
2017-10-09 15:10:49 +0200

    update

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,14 @@
 import firebase from 'firebase';
 
-//config
+const config = {
+  apiKey: "AIzaSyAdwEXpdSJ0iY1xwje5R3Vbv8c3a2APkfM",
+  authDomain: "qnh-app.firebaseapp.com",
+  databaseURL: "https://qnh-app.firebaseio.com",
+  projectId: "qnh-app",
+  storageBucket: "qnh-app.appspot.com",
+  messagingSenderId: "1027880630848"
+};
+firebase.initializeApp(config);
 
 export const provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
